41ecc6a315d920bb3abec58e5bc1b98045ce46d9
Williamsanchez-01 <williamcostasanchez@gmail.com>
2024-08-11 14:31:02 -0300

    exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Thread.Sleep(1000);
+Console.WriteLine("Preparar...");
+
+Thread.Sleep(1000);
+Console.WriteLine("Apontar...");
+
+Thread.Sleep(1000);
+Console.WriteLine("Fogo!");
